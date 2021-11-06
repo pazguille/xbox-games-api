7060dadf00ea07b548f0876a1d1598edbb7ca31c
Guille Paz <pazguille@Guilles-MacBook-Pro.local>
2021-11-05 22:53:58 -0300

    Improve images results.

--- dev/server.js
+++ dev/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const app = require('./app');
 
 const host = '0.0.0.0';
-const port = process.env.PORT || 3030;
+const port = process.env.PORT || 3031;
 
 http.createServer(app).listen(port, host, () => {
   console.log(`App listening on port ${port}.`);
--- utils/fetch-games-detail.js
+++ utils/fetch-games-detail.js
@@ -6,6 +6,19 @@ function fetchDetailFromMS(ids) {
   .catch(err => { throw { error: err.response.data.error }; });
 };
 
+function groupBy(xs, key) {
+  const group = xs.reduce(function(rv, x) {
+    (rv[x[key]] = rv[x[key]] || []).push(x);
+    return rv;
+  }, {});
+  Object.keys(group).forEach((key) => {
+    if (group[key].length === 1) {
+      group[key] = group[key][0];
+    }
+  });
+  return group;
+};
+
 async function fetchGamesDetail(ids) {
   const gamesResponse = await fetchDetailFromMS(
     Array.isArray(ids) ? ids : [ids]
@@ -20,10 +33,11 @@ async function fetchGamesDetail(ids) {
       deal: game.DisplaySkuAvailabilities[0].Availabilities[0].OrderManagementData.Price.ListPrice,
     },
     description: game.LocalizedProperties[0].ProductDescription,
-    images: game.LocalizedProperties[0].Images.map(img => ({ url: `https:${img.Uri}`, width: img.Width, height: img.Height, type: img.ImagePurpose.toLowerCase() })),
-    videos: game.LocalizedProperties[0].Videos.map(video => ({ url: video.Uri, width: video.Width, height: video.Height, poster: `https:${video.PreviewImage.Uri}` })),
+    images: groupBy(game.LocalizedProperties[0].Images.map(img => ({ url: `https:${img.Uri}`, width: img.Width, height: img.Height, type: img.ImagePurpose.toLowerCase() })), 'type'),
+    // videos: game.LocalizedProperties[0].Videos.map(video => ({ url: video.Uri, width: video.Width, height: video.Height, poster: `https:${video.PreviewImage.Uri}` })),
+    release_date: game.MarketProperties[0].OriginalReleaseDate,
+    related: game.MarketProperties[0].RelatedProducts,
   }));
-
   return games;
 };
 
