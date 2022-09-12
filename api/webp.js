6dfc380664c1353c266c953113f589c44cb326c5
pazguille <guille87paz@gmail.com>
2022-09-12 19:01:11 -0300

    Test webp converter.

--- api/webp.js
+++ api/webp.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const sharp = require('sharp');
 
 // https://store-images.s-microsoft.com/image/apps.2792.14572882104595488.f6337262-80e0-4705-aa02-3f9593b0fbd3.10050035-de9c-43b7-ae6e-83930c9ace2c?w=630
 // https://api.xstoregames.com/api/image/apps.2792.14572882104595488.f6337262-80e0-4705-aa02-3f9593b0fbd3.10050035-de9c-43b7-ae6e-83930c9ace2c?w=630
@@ -9,10 +10,19 @@ module.exports = async (req, res) => {
   const queryString = new URLSearchParams(req.query).toString();
   const microsoft = `https://store-images.s-microsoft.com/image/${path}?${queryString}`;
   const response = await axios.get(microsoft, { responseType: 'arraybuffer' });
-  res.setHeader('Content-Type', response.headers['content-type']);
-  res.setHeader('Content-Length', response.headers['content-length']);
+  // res.setHeader('Content-Type', response.headers['content-type']);
+  // res.setHeader('Content-Length', response.headers['content-length']);
 
   res.setHeader('Cache-Control', 'public, max-age=31536000, immutable');
   // res.setHeader('Cache-Control', 'public, max-age=0, s-maxage=31536000, stale-while-revalidate');
-  return res.status(200).send(response.data);
+
+  const data = await sharp(response.data)
+    .webp({ quality: 80 })
+    .toBuffer();
+
+  res.setHeader('Content-Type', 'image/webp');
+  res.setHeader('Content-Length', data.length);
+
+  return res.status(200).send(data);
+  // return res.status(200).send(response.data);
 };
