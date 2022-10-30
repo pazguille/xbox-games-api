ae4d5eaa767583867b37ddf82d578289aebe6607
pazguille <guille87paz@gmail.com>
2022-10-30 08:29:21 -0300

    Improve search results.

--- utils/fetch-search-games.js
+++ utils/fetch-search-games.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const fetchGamesDetail = require('./fetch-games-detail');
+// const fetchGamesDetail = require('./fetch-games-detail');
 
 const API_URL = 'https://www.microsoft.com/msstoreapiprod/api/autosuggest';
 
@@ -9,7 +9,7 @@ function fetchSearchGames(query, store, lang) {
     clientId: '7F27B536-CF6B-4C65-8638-A0F8CBDFCA65',
     sources: 'DCatAll-Products',
     filter: '+ClientType:StoreWeb',
-    counts: '5,0,0',
+    counts: '10,0,0',
     query,
   }})
   .then(response => response.data.ResultSets[0])
@@ -17,9 +17,20 @@ function fetchSearchGames(query, store, lang) {
     if (!data) { return Promise.reject(new Error()); }
     return data.Suggests
       .filter((result) => result.Source === 'Games')
-      .map((result) => result.Metas[0].Value);
+      .map((result) => ({
+        id: result.Metas[0].Value,
+        title: result.Title,
+        images: {
+          boxart: {
+            url: result.Metas[2].Value.replace('//store-images.s-microsoft.com', 'https://xbox-games-api.vercel.app/api'),
+            width: result.Metas[4].Value,
+            height: result.Metas[3].Value,
+            type: "boxart"
+          },
+        }
+      }));
   })
-  .then((games) => fetchGamesDetail(games, store, lang))
+  // .then((games) => fetchGamesDetail(games, store, lang))
   .catch(err => { throw { error: err }; });
 };
 
